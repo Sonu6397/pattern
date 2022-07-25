09f09efde5fdceb1c754462bcccf627a1f12bd7d
chhavi raj singh <chhavirajsingh2000@gmail.com>
2022-07-25 21:37:23 +0530

    Add project files.

---
+++ pattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 6;
+            for(int i=n; i>=1;i--)
+            {
+                for(int j=i;j>=1;j--)
+                {
+                    if (j == 1)
+                        Console.Write(" 1");
+                    else if (j == i)
+                        Console.Write(" " + n);
+                    else
+                        Console.Write(" " + j);
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
